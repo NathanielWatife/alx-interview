a52acd3fd116c4d965abb9b2667c456e93d7f52a
NathanielWatife <awosanminathniel0@gmail.com>
2024-10-03 16:31:10 +0100

    Worked on checker error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
+
 def pascal_triangle(n):
     """Generate Pascal's triangle up to the nth row."""
     if n <= 0:
         return []
-
-    triangle = []  # Initialize the list to hold all rows of the triangle
     
+    triangle = []
+
     for i in range(n):
         # Start each row with a 1
-        row = [1] * (i + 1)  # Each row has (i + 1) elements
-        
+        row = [1] * (i + 1)
         # Calculate the values for the internal elements (if any)
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
-        
         # Add the completed row to the triangle
         triangle.append(row)
-    
     return triangle
+
+def print_triangle(triangle):
+    """Print the row"""
+    for row in triangle:
+        print("[{}]".format("\n".join([str(x) for x in row])))
 
 # Testing the function
 if __name__ == "__main__":
